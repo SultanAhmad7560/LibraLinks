1b6ca0b7a881920d1d65d9395dc13e9b71f70e0d
Muhammad Mehdy <71124592+Mehdy922@users.noreply.github.com>
2023-10-06 01:08:54 +0500

    added main functionality

--- libgen.py
+++ libgen.py
@@ -1,10 +1,17 @@
 # search_title_filtered()
-
+import sys
+import json
 from libgen_api import LibgenSearch
+
+
+
 
 tf = LibgenSearch()
 title_filters = {"Extension": "pdf", "Language": "English"}
-titles = tf.search_title_filtered("Harry Potter and the prisoner of azkaban", title_filters, exact_match=True)
+titles = tf.search_title_filtered(str(sys.argv[1]), title_filters, exact_match=True)
 item_to_download = titles[0]
 download_links = tf.resolve_download_links(item_to_download)
-print(download_links)
+# Access individual URLs
+
+
+print(json.dumps(download_links))
